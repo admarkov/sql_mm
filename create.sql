d50834d61854a57fbf82f8eb80a71bc4bfa01e42
Alexander Markov <admarkov@lenta.ru>
2018-03-13 09:55:15 +0300

    Initial commit

---
+++ create.sql
@@ -0,0 +1,6 @@
+
+/*CREATE TABLE Chessman(
+	cid integer primary key check(cid>=0 and cid<32),
+	ftype char(6) NOT NULL check(ftype in ('king', 'queen', 'rook', 'bishop', 'knight', 'pawn')),
+	fcolor char(6) NOT NULL check(fcolor in ('black', 'white'))
+)*/
